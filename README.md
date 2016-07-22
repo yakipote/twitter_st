3a190abbc7b751919ae24b0bf054d01e8088084f
Ken Takahashi <takaken@redimpulz.com>
2016-07-23 03:30:56 +0900

    感情APIが使えた

---
+++ README.md
@@ -0,0 +1,6 @@
+### ライブラリの追加
+gem install bundler
+bundle install --path=vendor/bundle --binstubs=bundle_bin
+
+### 実行
+bundle exex ruby twitterstream.rb
--- twitterstream.rb
+++ twitterstream.rb
@@ -1,4 +1,7 @@
 require 'twitter'
+require 'net/http'
+require 'uri'
+require 'json'
 
 client = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = 'OSLJ9s4ep0223B0KH7lmIzGJ5'
@@ -7,11 +10,23 @@ client = Twitter::Streaming::Client.new do |config|
   config.access_token_secret = 'DawCand5hH4A9yFVbCbNAfiFEHMtmRL1t1g4QIJvg5LeT'
 end
 
+api_key = 'CA61BA3E57DE6FE4E20D693058C7FBE229A1A1BD'
+url = 'http://ap.mextractr.net/ma9/emotion_analyzer?out=json&apikey='
+
 client.user do |object|
   case object
   when Twitter::Tweet
-    p object.user.screen_name
-    p object.text
+    case object.user.screen_name
+    when 'zafnen'
+      p object.user.screen_name
+      p object.text
+      url_escape = URI.escape(url + api_key + '&text=' + object.text)
+      uri = URI.parse(url_escape)
+      json = Net::HTTP.get(uri)
+      result = JSON.parse(json)
+      p result
+      p URI.unescape(result['analyzed_text'])
+    end
   when Twitter::DirectMessage
     puts "It's a direct message!"
   when Twitter::Streaming::StallWarning
